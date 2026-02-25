bb56d12d1e70d3c6464077f16360de0ce9c3d62e
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2026-02-25 09:34:21 +0700

    set up

---
+++ src/main/java/gebxby/gebxbyblog/GebxByBlogApplication.java
@@ -0,0 +1,13 @@
+package gebxby.gebxbyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GebxByBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GebxByBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gebxby/gebxbyblog/GebxByBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package gebxby.gebxbyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GebxByBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
